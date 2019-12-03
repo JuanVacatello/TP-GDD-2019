7ce0f1e2c27aed561f06428b3ab329ce527374f5
lucilacanalini <39197752+lucilacanalini@users.noreply.github.com>
2019-12-03 18:12:20 -0300

    hola borre cosas inservibles

--- data/funcionalidadesyroles.sql
+++ data/funcionalidadesyroles.sql
@@ -1,13 +1,6 @@
 --use GD2C2019
                                                             /*Creacion de datos*/
 
-/*                         	Usuarios
-insert into SQL_INJECTION.Usuario (nombreUsuario,contraseña,tipoUsuario,estado) values ('admin','E6B87050BFCB8143FCB8DB0170A4DC9ED00D904DDD3E2A4AD1',1,1)
-GO
-insert into SQL_INJECTION.Usuario (nombreUsuario,contraseña,tipoUsuario,estado) values ('admin2','E6B87050BFCB8143FCB8DB0170A4DC9ED00D904DDD3E2A4AD1',1,1)
-GO
-*/
-
 --Funcionalidades
 
 -- 1) ABM ROL
@@ -56,6 +49,7 @@ INSERT INTO LIL_MIX.rol (rol_nombre) VALUES ('PROVEEDOR')
 GO
 
 --                          funcionalidadxrol
+
 -- Funciones Administrador
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (1,1)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (1,3)
@@ -66,36 +60,27 @@ INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (1,7)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (1,8)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (1,11)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (1,12) --No sabemos quien tiene esa función
+
 -- Funciones Cliente
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (2,2)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (2,5)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (2,6)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (2,9)
+
 -- Funciones Proveedor
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (3,2)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (3,6)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (3,8)
 INSERT INTO  LIL_MIX.funcionalidadxrol(rol_id,funcionalidad_id) VALUES (3,10)
 
-/*--                         RolesXUsuario
+--                       Listado de semestres
 
-INSERT INTO  LIL_MIX.rolxusuario(rol_id,usuario_id) VALUES (1,1)
-GO
-INSERT INTO  LIL_MIX.rolxusuario(rol_id,usuario_id) VALUES (2,2)
-GO
-INSERT INTO  LIL_MIX.rolxusuario(rol_id,usuario_id) VALUES (3,3)
-GO
-*/
---                       Listado estadistico
-INSERT INTO LIL_MIX.listado (listado_descripcion) VALUES ('Proveedores con mayor porcentaje de descuento ofrecido en sus ofertas')
-INSERT INTO LIL_MIX.listado (listado_descripcion) VALUES ('Proveedores con mayor facturaci�n')
-GO
---                       Listados de semestres
 INSERT INTO LIL_MIX.semestre(semestre_fecha_inicio, semestre_fecha_fin) VALUES ('01-01', '30-06')
 INSERT INTO LIL_MIX.semestre(semestre_fecha_inicio, semestre_fecha_fin) VALUES ('01-07', '31-12')
 GO
 
---                        Forma de pago
+--                        Formas de pago
+
 INSERT INTO LIL_MIX.tipoDePago(tipo_de_pago_descripcion) VALUES ('Efectivo')
 INSERT INTO LIL_MIX.tipoDePago(tipo_de_pago_descripcion) VALUES ('Credito')
 INSERT INTO LIL_MIX.tipoDePago(tipo_de_pago_descripcion) VALUES ('Debito')
